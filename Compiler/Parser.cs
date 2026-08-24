12e44d2bc1d71b82d5caba0e2574f12deff48b82
Nikita Grigorev <pettyaelse@gmail.com>
2026-08-25 03:16:28 +0700

    w

--- Compiler/Parser.cs
+++ Compiler/Parser.cs
@@ -238,10 +238,9 @@ public class Parser
         if (!Check(TokenType.LBrace))
         {
             Token given = _tokens[_cursor];
+            ReportError(given, TokenType.LBrace);
             GoTo(prevCursor, TokenType.LBrace, TokenType.KeywordFunc);
             MustBe(TokenType.LBrace);
-            // Report after MustBe to not report twice
-            ReportError(given, TokenType.LBrace);
         }
 
         Block body = ParseBlock();
