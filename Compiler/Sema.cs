10261a0c5cb0da13dec173a43acb3a3b65add18d
Nikita Grigorev <pettyaelse@gmail.com>
2026-08-25 23:50:14 +0700

    w

--- Compiler/Sema.cs
+++ Compiler/Sema.cs
@@ -5,13 +5,17 @@ public class Sema
     private string _code = "";
     private Diagnostic? _diag;
     private bool _hasErrors = false;
+    private List<Token> _tokens = [];
 
-    public bool Run(CompilationUnit unit, string code, Diagnostic diag)
+    public bool Run(CompilationUnit unit, string code, List<Token> tokens, Diagnostic diag)
     {
         _code = code;
         _diag = diag;
         _hasErrors = false;
+        _tokens = tokens;
+
         Run(unit);
+
         return _hasErrors;
     }
 
