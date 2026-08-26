77c605b3c4ca539e3616cac5a478296c43cd305c
Nikita Grigorev <pettyaelse@gmail.com>
2026-08-27 01:46:39 +0700

    w

--- Compiler/Sema.cs
+++ Compiler/Sema.cs
@@ -225,7 +225,7 @@ public class Sema
         foreach (Symbol sym in _allSymbols)
         {
             Token startToken = _tokens[sym.Declaration.StartToken];
-            string str = $"{startToken.Line,5}:{startToken.Column,-2} | {sym.Name,-10} | {sym.Type.Name}";
+            string str = $"{startToken.Line,5}:{startToken.Column,-2} | {sym.Name,-10} | {sym.Type.Name,-16}";
             if (sym.ScopeId != 0)
             {
                 str += $" | Scope: {_curScopeId}";
