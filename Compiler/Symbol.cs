85b16c0d1162c2e588bef05bd9746eeee5a7c70e
Nikita Grigorev <pettyaelse@gmail.com>
2026-08-27 01:46:09 +0700

    w

--- Compiler/Symbol.cs
+++ Compiler/Symbol.cs
@@ -6,7 +6,7 @@ public class Symbol
     public required string Name;
     public required Node Declaration;
 
-    public int ScopeId; // Debug only
+    public int ScopeId = -1; // Debug only
 
     public bool IsFunc => Type is FuncType;
     public bool IsBuiltin => Type is BuiltinType;
