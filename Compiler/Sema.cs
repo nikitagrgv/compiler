2b27cc5932be7e9f69d2af857a5c0f62ba62e67d
Nikita Grigorev <pettyaelse@gmail.com>
2026-09-02 01:08:21 +0700

    w

--- Compiler/Sema.cs
+++ Compiler/Sema.cs
@@ -151,7 +151,7 @@ public class Sema
 
         if (stmt.Target is not ExprIdentifier)
         {
-            // TODO: Support other
+            // TODO: Support other, lvalue
             Token token = _tokens[stmt.Target.StartToken];
             _diag?.AddError("Only an identifier can be a callee", token);
             _hasErrors = true;
